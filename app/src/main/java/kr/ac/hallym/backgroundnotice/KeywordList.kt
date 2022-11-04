c1b8ed2abc2c6af81aa46d51769b955001eb8767
CherryPichu <uskawjdu@gmail.com>
2022-11-04 18:45:01 +0900

    init git2

--- app/src/main/java/kr/ac/hallym/backgroundnotice/KeywordList.kt
+++ app/src/main/java/kr/ac/hallym/backgroundnotice/KeywordList.kt
@@ -3,19 +3,16 @@ package kr.ac.hallym.backgroundnotice
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import kr.ac.hallym.backgroundnotice.databinding.ActivityMainBinding
+import java.util.zip.Inflater
 
-class MainActivity : AppCompatActivity() {
+class KeywordList : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater);
-        setContentView(R.layout.activity_main)
-//        setContentView(R.layout.activity_keyword_list)
+        setContentView(R.layout.activity_keyword_list)
 
-        binding.ButtonGoKeyword.setOnClickListener {
-            val intent : Intent = Intent(this, KeywordList::class.java)
-            startActivity(intent)
-        }
     }
 }
